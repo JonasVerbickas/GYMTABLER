9c7d2701e612c61063a0d62ba4ea7488a844acaf
KenjiPcx <56083944+KenjiPcx@users.noreply.github.com>
2021-02-07 00:05:33 +0000

    Implemented simple algorithm, fixed some database requirements

--- main/models.py
+++ main/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 class Exercise(models.Model):
     name_ex = models.CharField(max_length = 100)
-    score = models.TextField()
+    score = models.IntegerField()
+    body_part = models.TextField()
+    reps = models.IntegerField()
 
+    class Meta:
+        db_table = "Exercises"
+
+    def __str__(self):
+        return self.name_ex
+    
