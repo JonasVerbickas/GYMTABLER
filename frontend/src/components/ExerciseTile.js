0b3b4ac3ab7e04fa4ce3c4e51e3f49a94f1d8377
Jonas Verbickas <jonasverbic@gmail.com>
2021-02-22 18:05:11 +0200

    Improve exercise tiles

--- frontend/src/components/ExerciseTile.js
+++ frontend/src/components/ExerciseTile.js
@@ -2,45 +2,74 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "../exerciseTile.css"
 
-class ExerciseTile extends React.Component{
-    constructor(props)
-    {
+const default_exercise = {
+    img: "https://www.muscletech.com/wp-content/uploads/muscletech-add-50-lbs-to-your-bench-press.jpg",
+    title: "Placeholder title",
+    difficulty: "X",
+    equipment: "None"
+}
+
+
+class ExerciseTile extends React.Component {
+    constructor(props) {
         super(props);
+        let adjusted_exercise = {};
+        Object.keys(default_exercise).forEach(function (key) {
+            adjusted_exercise[key] = props.exercise[key] ? props.exercise[key] : default_exercise[key];
+        })
         this.state = {
-            img: props.img,
-            title: props.title,
+            img: adjusted_exercise.img,
+            title: adjusted_exercise.title,
+            difficulty: adjusted_exercise.difficulty,
+            equipment: adjusted_exercise.equipment,
             expanded: false
         }
     }
 
-    render(){
-        let class_name;
-        if(!this.state.expanded)
-        {
-            class_name = "exercise-tile small-exercise-tile";
+    render() {
+        if (!this.state.expanded) {
+            let class_name = "exercise-tile small-exercise-tile";
+            return (
+                <div className={class_name} onClick={() => this.setState({ expanded: !this.state.expanded })}>
+                    <img src={this.state.img} alt={this.state.title}></img>
+                    <p className="exercise-title">{this.state.title}</p>
+                </div>
+            );
         }
-        else
-        {
-            class_name = "exercise-tile expanded-exercise-tile";
+        else {
+            let class_name = "exercise-tile expanded-exercise-tile";
+            return (
+                <div className={class_name} onClick={() => this.setState({ expanded: !this.state.expanded })}>
+                    <img src={this.state.img} alt={this.state.title}></img>
+                    <p className="exercise-title">{this.state.title}</p>
+                    <div className="exercise-stats">
+                        <div className="exercise-stats-row">
+                            <p className="exercise-description">A short description about the exercise</p>
+                        </div>
+                        <div className="exercise-stats-row">
+                            <p>Difficulty level: {this.state.difficulty}</p>
+                            <p>Equipment needed: {this.state.equipment}</p>
+                        </div>
+                        <div className="exercise-stats-row muscle-group-row">
+                            <p>Muscle groups:</p>
+                            <ul className="muscle-group-list"> 
+                                <li>KA</li>
+                                <li>BOOM</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            );
         }
-        return (
-            <div className={class_name} onClick={() => this.setState({expanded: !this.state.expanded})}>
-                <img src={this.state.img} alt={this.state.title}></img>
-                <p>{this.state.title}</p>
-            </div>
-        );
     }
-    
 }
 
 ExerciseTile.defaultProps = {
-    img: "https://www.muscletech.com/wp-content/uploads/muscletech-add-50-lbs-to-your-bench-press.jpg",
-    title: "Placeholder title",
+    exercise: default_exercise
 }
 
 ExerciseTile.propTypes = {
-    img: PropTypes.string,
-    title: PropTypes.string
+    exercise: PropTypes.object,
 }
 
 export default ExerciseTile;
--- frontend/src/components/ExerciseTileColumn.js
+++ frontend/src/components/ExerciseTileColumn.js
@@ -3,7 +3,6 @@ import ExerciseTile from './ExerciseTile.js';
 export default function ExerciseTileColumn(props)
 {
     return (<div className="exercise-tile-column">
-        {props.listOfExercises.map((exercise, index) => <ExerciseTile key={"exercise_tile"+index} title={exercise.title} img={exercise.img}/>)}
+        {props.listOfExercises.map((exercise, index) => <ExerciseTile key={"exercise_tile"+index} exercise={exercise}/>)}
     </div>)
-
 }
