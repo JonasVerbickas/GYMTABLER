21cdda0b7f143e0d21e5424c4e436b1abb0e8976
KenjiPcx <56083944+KenjiPcx@users.noreply.github.com>
2021-03-04 13:24:10 +0000

    Modified Workout Table

--- workouts/admin.py
+++ workouts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from workouts.models import Workout
 
 class WorkoutAdmin(admin.ModelAdmin):
-    list_display = ['account', 'workout_title', 'workout_content', 'date_created', 'slug']
+    list_display = ['account', 'workout_title', 'date_created', 'slug']
     search_fields = ['account']
     readonly_fields= ('slug', 'date_created')
 
--- workouts/models.py
+++ workouts/models.py
@@ -3,10 +3,12 @@ from django.db import models
 from django.utils.text import slugify
 from django.conf import settings
 from django.db.models.signals import pre_save
+from main.models import Exercise
 
 class Workout(models.Model):
     workout_title = models.CharField(max_length=50, verbose_name="Workout Title", null=False)
-    workout_content = models.TextField(max_length=5000, verbose_name="Workout", null=False)
+    workout_exercises = models.ManyToManyField(Exercise)
+    workout_description = models.TextField(max_length=250, verbose_name="Description")
     date_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField(blank=True, unique=True)
