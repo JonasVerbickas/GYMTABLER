429867b2632dd56369835ac77c8164b8c5588e40
Jonas Verbickas <jonasverbic@gmail.com>
2021-03-05 12:59:40 +0200

    Instead of completely removing filtered exercises just display:none

--- frontend/src/components/exerciseTiles/ExerciseTileTable.js
+++ frontend/src/components/exerciseTiles/ExerciseTileTable.js
@@ -2,18 +2,21 @@ import "../../assets/css/exerciseTable.css";
 import ExerciseTile from "./ExerciseTile";
 import PropTypes from 'prop-types';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import filteredExercises from "./exerciseFiltering.js";
+import filteredExerciseMask from "./exerciseFiltering.js";
 
 
 export default function ExerciseTileTable(props) {
-    let filtered_exercises = filteredExercises(props.listOfExercises, props.filter);
+    let filtered_exercise_mask = filteredExerciseMask(props.listOfExercises, props.filter);
     // map breakpoints where the number of columns displayed increases
-    let breakpoints = { 400: 1, 800: 2, 1200: 3, 1600: 4, 2000: 5 } 
-    if (filtered_exercises.length)
+    let breakpoints = { 400: 1, 800: 2, 1200: 3, 1600: 4, 2000: 5 };
+    if (filtered_exercise_mask.includes(true))
     {
         return (<ResponsiveMasonry columnsCountBreakPoints={breakpoints}>
             <Masonry className="exercise-tile-table">
-                {filtered_exercises.map((exercise) => (<ExerciseTile key={exercise.name} exercise={exercise} addToCart={props.addToCart} />))}
+                {props.listOfExercises.map((exercise, index) => {
+                    return (<div style={{ display: filtered_exercise_mask[index] ? 'inherit' : 'none'}}><ExerciseTile key={exercise.name} exercise={exercise} addToCart={props.addToCart} /></div>);
+                    })
+                }
             </Masonry></ResponsiveMasonry>)
     }
     else
