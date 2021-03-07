53cd2ffdc2ff9e038dd82fbb8eef52c5b5d2e2ac
Jonas Verbickas <jonasverbic@gmail.com>
2021-03-07 17:32:12 +0200

    Make button disable logic neater

--- frontend/src/components/exerciseTiles/exerciseCart.js
+++ frontend/src/components/exerciseTiles/exerciseCart.js
@@ -5,6 +5,6 @@ export default function ExerciseCart(props) {
     return (<div id="exercise-cart">
         <h4>Exercise cart:</h4>
         {props.cart.length > 0 ? <ul>{props.cart.map((exercise_in_cart) => (<CartItem key={exercise_in_cart} name={exercise_in_cart} removeFromCart={() => props.removeFromCart(exercise_in_cart)}/>))}</ul> : <h6>No exercises added</h6>}
-        { props.cart.length > 0 ? <button className="continue-button">Continue</button> : <button disabled className="continue-button">Continue</button> }
+        <button disabled={props.cart.length === 0} className="continue-button">Continue</button>
     </div>);
 }
