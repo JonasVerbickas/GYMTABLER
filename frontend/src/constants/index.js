bd0e14b7aa35dc7410baffe4a53401352e58ffc5
Alex Constantin <alex.constantin432@gmail.com>
2021-03-12 18:45:35 +0000

    login integration on frontend

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,3 +1,3 @@
 export const API_URL = "http://localhost:8000/api/get_workout/";
-export const REGISTER_URL = "http://127.0.0.1:8000/register";
-export const LOGIN_URL = "http://127.0.0.1:8000/login";
+export const REGISTER_URL = "http://127.0.0.1:8000/dj-rest-auth/registration/";
+export const LOGIN_URL = "http://127.0.0.1:8000/dj-rest-auth/login/";
